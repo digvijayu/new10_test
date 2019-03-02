0b9fe9c47a2c8ea87cda06f84b0d9a16205cbf46
Digvijay Upadhyay <digvijay.upadhyay@globant.com>
2019-03-02 14:13:59 +0000

    production build

---
+++ build/precache-manifest.57ca9c9cc2a2cfb235d20acbedf77910.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6aba35fb9ef2fc8ff87c",
+    "url": "/static/css/main.6a998285.chunk.css"
+  },
+  {
+    "revision": "6aba35fb9ef2fc8ff87c",
+    "url": "/static/js/main.6aba35fb.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "007c8bf1545160b838d2",
+    "url": "/static/js/2.007c8bf1.chunk.js"
+  },
+  {
+    "revision": "419df5c581605d94afcd0bfaacaa6b71",
+    "url": "/index.html"
+  }
+];
